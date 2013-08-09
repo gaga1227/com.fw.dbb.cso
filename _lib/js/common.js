e117294e87ff5b384885e47e5aef82ebd73273b7
Johnny Xu <gaga1227@gmail.com>
2013-08-09 15:27:04 +1000

    cleaned up JS, adding web font loader

--- _lib/js/common.js
+++ _lib/js/common.js
@@ -1,159 +1,26 @@
 /* ------------------------------------------------------------------------------ */
-/* initFocusAccordion */
+/* webfonts */
 /* ------------------------------------------------------------------------------ */
-function initFocusAccordion(){
-
-	//vars
-	var $container = $('#focus'),
-		$accordions = $container.find('.accordion'),
-		$btns = $container.find('.btnTitle'),
-		activeClasses = [ 'activeA', 'activeB', 'activeC' ],
-		activeIndex = 0,
-		btnActiveCls = 'active',
-		
-		//function - setActive
-		setActive = function(idx){
-			//update activeIndex with supplied index
-			activeIndex = idx; 
-			//update container active class
-			$container
-				.removeClass( activeClasses.join(' ') )
-				.addClass( activeClasses[activeIndex] );
-			//update trigger button status
-			$.each($accordions, function(i, ele){
-				var $accordion = $(ele),
-					$btn = $accordion.find('.btnTitle');
-				if ( i == idx ) {
-					$accordion.attr('data-active', '1');
-					$btn.addClass(btnActiveCls);	
-				} else {
-					$accordion.attr('data-active', '0');
-					$btn.removeClass(btnActiveCls);	
-				}
-			});
-		};
-	
-	//process each inctances
-	$.each($accordions, function(idx, ele){
-		//vars
-		var $ele = $(ele), //instance
-			$btn = $ele.find('.btnTitle'), //trigger btn
-			defaultaActive = $ele.attr('data-active') == 1 ? true : false; //default active flag	
-		
-		//update activeIndex if default active flag is found
-		if ( defaultaActive ) activeIndex = idx;
-		
-		//enable trigger button
-		$btn.on('click', function(e){
-			e.preventDefault();
-			if ( defaultaActive ) {
-				return;			//ignore if already active
-			} else {
-				setActive(idx);	//update with new active index
-			}
-		});
-	});
-	
-	//init set active instance based on activeIndex
-	setActive(activeIndex);
-	
-}
-/* ------------------------------------------------------------------------------ */
-/* initNewsSlideshow */
-/* ------------------------------------------------------------------------------ */
-function initNewsSlideshow(){
-	
-	//vars
-	var	$news = $('#news'),
-		$slideshow = $news.find('.newsSlideshow'),
-		$btnPrev = $news.find('.btnPrev'),
-		$btnNext = $news.find('.btnNext'),
-		initiated = destroyed = slideshowMode = false,
-		newsSlideshow;
-	
-	//init slideshow
-	function init(){
-		//init media interaction
-		newsSlideshow = new initSlideshows('.newsSlideshow');
-		
-		//hook up slideshow with independent buttons
-		$btnPrev.bind('click', function(e){
-			e.preventDefault();
-			$slideshow.cycle('prev');	
-		});
-		$btnNext.bind('click', function(e){
-			e.preventDefault();
-			$slideshow.cycle('next');		
-		});
-		
-		//update status
-		initiated = true;
-		destroyed = false;
-		
-		//console.log('initiated');
-	}
-	
-	//destroy slideshow
-	function destroy(){
-		//destroy interaction
-		$slideshow.cycle('destroy');
-		
-		//clean up
-		$slideshow.find('.slide').removeAttr('style');
-		
-		//unhook up slideshow with independent buttons
-		$btnPrev.unbind('click');
-		$btnNext.unbind('click');
-		
-		//update status
-		initiated = false;
-		destroyed = true;
-		
-		//console.log('destroyed');
-	}
-	
-	//update slideshow
-	function update(){
-		//determine mode
-		//IE has no mq support, manually test
-		if ($('html').hasClass('oldie')) {
-			slideshowMode = ( $(window).innerWidth() > 500 ) ? true : false;
-			//console.log('slideshowMode ', $(window).innerWidth());
-		} else {
-			slideshowMode = !Modernizr.mq(mqStates.max500);	
-		}
-		//destroy or init
-		if ( slideshowMode && !initiated ) {
-			init();
-		} else if ( !slideshowMode && !destroyed ) {
-			destroy();
-		}
-		//console.log('mode ', slideshowMode);
-	}
-
-	//update on window resize
-	$(window).resize(function(e) {
-		update();
-	});
-	
-	//init the whole thing
-	update();
-		
-}
+WebFontConfig = { 
+	google: 		{ families: [ 'Merriweather:400,700:latin', 'Montserrat:400,700:latin' ] },
+	loading: 		function() { console.log('wf loading'); },
+	active: 		function() { console.log('wf active'); },
+	inactive: 		function() { console.log('wf inactive'); },
+	fontloading: 	function( familyName, fvd ) { console.log( familyName, fvd, 'loading' ); },
+	fontactive: 	function( familyName, fvd ) { console.log( familyName, fvd, 'active' ); },
+	fontinactive: 	function( familyName, fvd ) { console.log( familyName, fvd, 'inactive' ); } 
+};
 /* ------------------------------------------------------------------------------ */
 /* init */
 /* ------------------------------------------------------------------------------ */
-var SelectNav, Slideshows, StaticAudios, StaticVideos, Megamenu;
+var SelectNav, Slideshows, StaticAudios, StaticVideos;
 function init(){
 	
 	//layout assistance
-	insertFirstLastChild('#navItems, #sideNav, #sideNav ul, #megafooter > .padder, .stackListing');
+	insertFirstLastChild('#navItems, #sideNav, #sideNav ul');
 	
 	//interactions	
 	SelectNav = new initSelectNav();
-	Megamenu = new initMegamenu();
-	initBtnScroll();
-	initSharingButtons();
 	
 	//media
 	Slideshows = initSlideshows();
@@ -162,11 +29,7 @@ function init(){
 	if ( $('body#home').length ) {
 		initHome();
 	} else {
-		//enhance form placeholder for old browsers
-		$('input, textarea').placeholder();
-		
-		//initDatepicker
-		initDatepicker();
+		//enhance forms
 		
 		//media
 		StaticAudios = new initStaticAudios();
@@ -183,20 +46,11 @@ function init(){
 function initHome(){
 	
 	//layout assistance
-	insertFirstLastChild('.newsSlideshow');
-	
-	//focus accordion
-	initFocusAccordion();
-	
-	//news slideshow
-	initNewsSlideshow();
-	
-	//enhance form placeholder for old browsers
-	$('#formENews').find('input').placeholder();
 	
 }
 /* DOM.ready */
-$(document).ready(function(){ 
+$(document).ready(function(){
+	initWebFontLoader();
 	Platform.addDOMClass();
 	init();	
 });
--- _lib/js/fw.utils.js
+++ _lib/js/fw.utils.js
@@ -184,6 +184,20 @@ function initBtnScroll( cls ) {
 		});
 	});	
 }
+/* ------------------------------------------------------------------------------ */
+/* common - initWebFontLoader */
+/* ------------------------------------------------------------------------------ */
+function initWebFontLoader() {
+	(function() {
+		var wf = document.createElement('script');
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	})();
+}
 
 
 
