ce8fc079da6519402e5ce1cd007c55b920b9cdeb
Johnny Xu <gaga1227@gmail.com>
2013-08-26 13:35:18 +1000

    m6: Landing page/isotope
    added isotope plugin (js,css) with center align patch code

--- _lib/js/common.js
+++ _lib/js/common.js
@@ -11,6 +11,52 @@ WebFontConfig = {
 	fontinactive: 	function( familyName, fvd ) { console.log( familyName, fvd, 'inactive' ); } 
 };
 /* ------------------------------------------------------------------------------ */
+/* initIsotope */
+/* ------------------------------------------------------------------------------ */
+function initIsotope(){
+	//vars
+	var $container = $('.isotopeContainer'),
+		colW = 320,
+		isotopeIsOn = false;
+	//update handler
+	function update(){
+		//check mq
+		var isotopeOn = !Modernizr.mq(mqStates.max400);
+		//toggle isotope
+		if (isotopeOn) {
+			//check flag
+			if (isotopeIsOn) return '[isotope] already on';
+			//update flag
+			isotopeIsOn = true;
+			//imagesLoaded
+			$container.imagesLoaded(function(){
+				//isotope
+				$container.isotope({
+					itemSelector:	'.item.tile',
+					layoutMode:		'masonry',
+					masonry:		{ columnWidth: colW }
+				}, function($items){
+					console.log('[isotope] anim complete');	
+				});
+				console.log('[isotope] initialised');
+				
+			});	
+		} else {
+			//check flag
+			if (!isotopeIsOn) return '[isotope] already off';
+			//update flag
+			isotopeIsOn = false;
+			//destroy
+			$container.isotope('destroy');
+			console.log('[isotope] destroyed');
+		}	
+	}
+	//init call
+	update();
+	//bind update to window resize
+	$(window).bind('resize', update);
+}
+/* ------------------------------------------------------------------------------ */
 /* init */
 /* ------------------------------------------------------------------------------ */
 var SelectNav, Slideshows, StaticAudios, StaticVideos;
@@ -28,6 +74,9 @@ function init(){
 	//template specific functions
 	if ( $('body#home').length ) {
 		initHome();
+	}
+	if ( $('body.landing').length ) {
+		initLanding();
 	} else {
 		//media
 		StaticAudios = new initStaticAudios();
@@ -46,6 +95,12 @@ function initHome(){
 	//layout assistance
 	
 }
+function initLanding(){
+	
+	//isotope tiles
+	initIsotope();
+	
+}
 /* DOM.ready */
 $(document).ready(function(){
 	console.log('dom ready');
@@ -53,3 +108,7 @@ $(document).ready(function(){
 	Platform.addDOMClass();
 	init();	
 });
+
+
+
+
